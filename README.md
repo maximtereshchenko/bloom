4e96bd925df4ada703e33c74933291a9ed47bfde
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-08-24 16:02:35 +0200

    README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bloom
+
+CHIP-8 interpreter (or emulator).
+
+## Description
+
+Created in 1977, CHIP-8 is the original fantasy console. Initially designed to ease game development for the COSMAC VIP
+kit computer, it has enjoyed several revivals over the decades for new and exciting platforms. Today, creating a CHIP-8
+implementation is a rite of passage for anyone interested in learning about emulation.
+
+## Used resources
+
+* [Guide to making a CHIP-8 emulator](https://tobiasvl.github.io/blog/write-a-chip-8-emulator)
+* [Cowgod's Chip-8 Technical Reference v1.0](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM)
+* [A collection of ROM images with tests](https://github.com/Timendus/chip8-test-suite)
+
+## How to use
+
+1. Package the code into a JAR:
+    ```shell
+   mvn package
+   ```
+2. Execute the CHIP-8 program using the packaged emulator:
+    ```shell
+   java -jar ./target/bloom-1.0-SNAPSHOT.jar <path to your program>
+   ```
--- src/test/java/com/github/maximtereshchenko/bloom/application/ApplicationTests.java
+++ src/test/java/com/github/maximtereshchenko/bloom/application/ApplicationTests.java
@@ -16,6 +16,115 @@ import java.util.Objects;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+/**
+ * <ul>
+ *     <li>
+ *         1-chip8-logo - this first test can tell you if you're interpreting these opcodes properly:
+ *         <ul>
+ *             <li>00E0 - Clear the screen</li>
+ *             <li>6xnn - Load normal register with immediate value</li>
+ *             <li>Annn - Load index register with immediate value</li>
+ *             <li>Dxyn - Draw sprite to screen (only aligned)</li>
+ *             <li>1nnn - Jump</li>
+ *         </ul>
+ *     </li>
+ *     <li>
+ *         2-ibm-logo - if you can see the IBM logo, you are properly interpreting these opcodes:
+ *         <ul>
+ *             <li>00E0 - Clear the screen</li>
+ *             <li>6xnn - Load normal register with immediate value</li>
+ *             <li>Annn - Load index register with immediate value</li>
+ *             <li>7xnn - Add immediate value to normal register</li>
+ *             <li>Dxyn - Draw sprite to screen (un-aligned)</li>
+ *             <li>1nnn - Jump</li>
+ *         </ul>
+ *     </li>
+ *     <li>
+ *         3-opcodes - the codes on the screen correspond to the functioning of these opcodes:
+ *         <table>
+ *             <tr>
+ *                 <td>3xnn</td>
+ *                 <td>2nnn</td>
+ *                 <td>8xy4</td>
+ *                 <td>Fx55</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>4xnn</td>
+ *                 <td>00EE</td>
+ *                 <td>8xy5</td>
+ *                 <td>Fx33</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>5xy0</td>
+ *                 <td>8xy0</td>
+ *                 <td>8xy7</td>
+ *                 <td>Fx1E</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>7xnn</td>
+ *                 <td>8xy1</td>
+ *                 <td>8xy6</td>
+ *                 <td>Registers*</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>9xy0</td>
+ *                 <td>8xy2</td>
+ *                 <td>8xyE</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>1nnn</td>
+ *                 <td>8xy3</td>
+ *                 <td>Fx65</td>
+ *             </tr>
+ *         </table>
+ *         *The v0 - vF registers should be 8 bits wide. This tests to see if it can overflow your registers.
+ *     </li>
+ *     <li>
+ *         4-flags - This test checks to see if your math operations function properly on some given set of input values.
+ *         But more importantly: it checks to see if you set the flag register vF properly when running those opcodes,
+ *         and if you don't mess up vF too early (when vF is used as one of the operands).
+ *         This is often an issue as the flags are pretty unintuitive and fairly hard to debug.
+ *         Each code on the screen corresponds to an opcode, and shows if the output value is correct (first checkmark),
+ *         if the flag in vF is correct (second checkmark), if vF can be used as the vY input (third checkmark)
+ *         and if vF can be used as the vX input (fourth checkmark, where present).
+ *         The top part checks the behaviour of the following opcodes, in the case where we don't expect an overflow,
+ *         carry or shifted out bit:
+ *         <table>
+ *             <tr>
+ *                 <td>HAPPY</td>
+ *                 <td>8xy1</td>
+ *                 <td>8xy2</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>8xy3</td>
+ *                 <td>8xy4</td>
+ *                 <td>8xy5</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>8xy6</td>
+ *                 <td>8xy7</td>
+ *                 <td>8xyE</td>
+ *             </tr>
+ *         </table>
+ *         The bottom part checks behaviour of the following opcodes, in the case that there is an overflow,
+ *         carry or shifted out bit:
+ *         <table>
+ *             <tr>
+ *                 <td>CARRY</td>
+ *                 <td>8xy4</td>
+ *                 <td>8xy5</td>
+ *             </tr>
+ *             <tr>
+ *                 <td>8xy6</td>
+ *                 <td>8xy7</td>
+ *                 <td>8xyE</td>
+ *             </tr>
+ *         </table>
+ *         The last row checks that the opcode Fx1E (i += vX) properly adds the value of register vX to the index
+ *         register, first for a regular register and then when using vF as the vX input register.
+ *     </li>
+ * </ul>
+ */
 final class ApplicationTests {
 
     private static final String MOVE_CURSOR_34_LINES_UP = "\033[34F";
