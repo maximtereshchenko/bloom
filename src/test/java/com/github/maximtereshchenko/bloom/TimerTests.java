ae2d3b424b47e2873d6a9cf03387a90b38ab4cac
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-08-27 10:49:38 +0200

    TimerTests

---
+++ src/test/java/com/github/maximtereshchenko/bloom/TimerTests.java
@@ -0,0 +1,40 @@
+package com.github.maximtereshchenko.bloom;
+
+import com.github.maximtereshchenko.bloom.api.DecrementDelayTimerUseCase;
+import com.github.maximtereshchenko.bloom.api.ExecuteNextOperationUseCase;
+import org.junit.jupiter.api.Test;
+
+final class TimerTests {
+
+    @Test
+    void givenDecrementSoundTimer_thenSoundDisabled() {
+        new Dsl()
+            .givenProgram(
+                new Set('0', "01"),
+                new SetSoundTimer('0')
+            )
+            .whenDecrementSoundTimer()
+            .thenSoundEnabled(false);
+    }
+
+    @Test
+    void givenDecrementDelayTimer_thenDelayTimerDecremented() {
+        new Dsl()
+            .givenProgram(
+                new Set('0', "01"),
+                new SetDelayTimer('0'),
+                new ReadDelayTimer('1'),
+                new SetFontCharacter('1'),
+                new Display('2', '2', 5)
+            )
+            .when(
+                ExecuteNextOperationUseCase::executeNextOperation,
+                ExecuteNextOperationUseCase::executeNextOperation,
+                DecrementDelayTimerUseCase::decrementDelayTimer,
+                ExecuteNextOperationUseCase::executeNextOperation,
+                ExecuteNextOperationUseCase::executeNextOperation,
+                ExecuteNextOperationUseCase::executeNextOperation
+            )
+            .thenOutputMatchesExpectation();
+    }
+}
